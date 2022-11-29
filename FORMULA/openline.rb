ed88bde96a990285ecf2d1363735353299a25f6b
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2022-11-29 14:43:31 +0000

    Update openline.rb

--- FORMULA/openline.rb
+++ FORMULA/openline.rb
@@ -1,15 +1,22 @@
 class Openline < Formula
     desc "Everything you need to get started with Openline"
     homepage "https://openline.ai/docs"
-    url "https://openline.sh/openline-v0.4.3-f136c4b-linux-arm.tar.xz"
     license "Apache-2.0"
-
+    url "https://openline.sh/openline-v0.4.3-f136c4b-linux-arm.tar.xz"
+   
     # to get the latest SHA:
     # find latest release: aws s3api list-objects --bucket openline.sh
     # download release: aws s3 cp [s3:// address of tarball] . 
     # get sha: shasum -a 256 [filename]
   
     sha256 "93e829ed12dfbe3ef644c86036b9d899d2bb702da58f99b59310e90e25a02849"
+    
+    depends_on "colima"
+    depends_on "docker"
+    depends_on "git"
+    depends_on "helm"
+    depends_on "kubectl"
+    depends_on "xcode
 
     def install
         inreplace "bin/openline", /^CLIENT_HOME=/, "export OPENLINE_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
