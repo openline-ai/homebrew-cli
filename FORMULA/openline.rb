932fd050aaed68a1332490707ccac9a891a4a95d
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2022-12-16 13:20:08 +0000

    Update openline.rb

--- FORMULA/openline.rb
+++ FORMULA/openline.rb
@@ -2,14 +2,14 @@ class Openline < Formula
     desc "Everything you need to get started with Openline"
     homepage "https://openline.ai/docs"
     license "Apache-2.0"
-    url "https://openline.sh/openline-v0.4.3-f136c4b-linux-arm.tar.xz"
+    url "http://openline.sh/channels/stable/openline-darwin-arm64.tar.gz"
    
     # to get the latest SHA:
     # find latest release: aws s3api list-objects --bucket openline.sh
     # download release: aws s3 cp [s3:// address of tarball] . 
     # get sha: shasum -a 256 [filename]
   
-    sha256 "93e829ed12dfbe3ef644c86036b9d899d2bb702da58f99b59310e90e25a02849"
+    sha256 "08fa2203e4db4867329ffd2c0d093fac8ab3102c99d7cc4a3addec02edccfafb"
     
     depends_on "colima"
     depends_on "docker"
@@ -18,8 +18,8 @@ class Openline < Formula
     depends_on "kubectl"
 
     def install
-        bin.install "openline"
         inreplace "bin/openline", /^CLIENT_HOME=/, "export OPENLINE_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+        inreplace "bin/openline", "\"$DIR/node\"", Formula["openline-node"].opt_bin/"node"
         libexec.install Dir["*"]
         bin.install_symlink libexec/"bin/openline"
     end
