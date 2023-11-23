44be802513ac3708d1594dee0efbee64b64b0d65
SilviuOpenline <125381623+SilviuOpenline@users.noreply.github.com>
2023-11-23 08:46:29 +0000

    Update openline.rb

--- FORMULA/openline.rb
+++ FORMULA/openline.rb
@@ -9,7 +9,7 @@ class Openline < Formula
     # download release: aws s3 cp [s3:// address of tarball] . 
     # get sha: shasum -a 256 [filename]
   
-    sha256 "fa9b3cf0e30a55ae0c35d332bc1014d5bc6939d5f00a76fbde1cca9094270b30"
+    sha256 "037b397bb2c986261210ea94f663942e7edc81ec4e2a71e19fa55209a7eed724"
     
     depends_on "colima"
     depends_on "docker"
